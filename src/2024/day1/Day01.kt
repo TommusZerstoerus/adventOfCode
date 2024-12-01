938513b120611f15c942756cfcc5a3f07340667f
Tom <78365287+TommusZerstoerus@users.noreply.github.com>
2024-12-01 08:25:47 +0100

    Add day01

--- src/2024/day1/Day01.kt
+++ src/2024/day1/Day01.kt
@@ -1,8 +1,47 @@
 package `2024`.day1
 
 import java.io.File
+import kotlin.math.abs
 
 fun main() {
-    val input = File("src/day8/input.txt").readLines()
-    println("Lösung")
+    val input = File("src/2024/day1/input.txt").readLines()
+    val sample = File("src/2024/day1/sample.txt").readLines()
+    val part1 = part1(input)
+    println(part1)
+    val part2 = part2(input)
+    println(part2)
+}
+
+fun part1(lines: List<String>): Int {
+    val pairs = lines.map { line ->
+        val (a, b) = line.split("\\s+".toRegex()).map { it.toInt() }
+        a to b
+    }
+
+    val sortedLeft = pairs.map { it.first }.sorted()
+    val sortedRight = pairs.map { it.second }.sorted()
+
+    val differences = sortedLeft.zip(sortedRight).map { (left, right) ->
+        abs(left - right)
+    }
+
+    return differences.sum()
+}
+
+fun part2(lines: List<String>): Int {
+    val pairs = lines.map { line ->
+        val (a, b) = line.split("\\s+".toRegex()).map { it.toInt() }
+        a to b
+    }
+
+    val leftList = pairs.map { it.first }
+    val rightList = pairs.map { it.second }
+
+    val resultList = leftList.map { left ->
+        val count = rightList.count { it == left }
+        left * count
+    }
+
+    return resultList.sum()
+
 }
